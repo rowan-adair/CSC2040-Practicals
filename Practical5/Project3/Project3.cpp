8f908fc2ae005f512231bcc28d39fdeb0c8b31c2
The-Boat <radair2842@gmail.com>
2019-11-06 10:54:28 +0000

    Practical Up To Date

--- Practical5/Project3/Project3.cpp
+++ Practical5/Project3/Project3.cpp
@@ -25,23 +25,30 @@ void userSearchSet(set<int> iset) {
 	}
 }
 
-int main() {
-
-	set<int> iset;
-	iset.insert(5);
-	iset.insert(9);
-	iset.insert(1);
-	iset.insert(8);
-	iset.insert(3);
-	cout << "iset contains:";
-	printSet(iset);
-
-	userSearchSet(iset);
-
+set<int> userInsert(set<int> iset) {
 	int newVal = 0;
 	cout << "Enter a new integer value:";
 	cin >> newVal;
 	iset.insert(newVal);
+	cout << "New set : " << printSet(iset);
+	return iset;
+}
+
+int main() {
+
+	set<int> iset;
+	for (size_t i = 1; i <= 10; i++)
+	{
+		iset.insert(i);
+	}
+	cout << "iset contains:";
+	printSet(iset);
+
+	// User search for value.
+	userSearchSet(iset);
+
+	// User inserts value
+	iset = userInsert(iset);
 	printSet(iset);
 
 	return 0;
