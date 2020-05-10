7a8e9b40f32cf303a608f9b419869220c64ac8d7
The-Boat <radair2842@gmail.com>
2020-05-10 21:28:48 +0100

    Removed Old Files

--- Assessement_Practice/Task_1/part_1.h
+++
@@ -1,5 +0,0 @@
-
-double get_mid(int a, int b, int c);
-void reverse(double& u, double& v, double& x, double& y);
-
-
--- Assessment2_Practice/Assessment2_Practice/search_r.h
+++
@@ -1,5 +0,0 @@
-#ifndef SEARCH_R_H
-#define SEARCH_R_H
-
-
-#endif // !SEARCH_R_H
